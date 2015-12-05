4c739fd2127e7165f37bef58a57e474d0e80e4ba
garicchi <xgaricchi@gmail.com>
2015-12-05 16:52:49 +0900

    BasicTemplate追加

---
+++ BasicTemplate/BasicTemplate/App.xaml.cs
@@ -0,0 +1,116 @@
+﻿using BasicTemplate.Commons;
+using BasicTemplate.Pages;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace BasicTemplate
+{
+    sealed partial class App : Application
+    {
+        public static AppStateManager StateManager { get; set; }
+        public static event Action<AppState, AppState> OnChangeAppState;
+
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+            StateManager = new AppStateManager();
+            StateManager.StateList.Add(AppState.Mobile, 0);
+            StateManager.StateList.Add(AppState.Normal, 800);
+            StateManager.StateList.Add(AppState.Wide, 1600);
+
+            OnChangeAppState += (s, s2) => { };
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+
+#if DEBUG
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                this.DebugSettings.EnableFrameRateCounter = true;
+            }
+#endif
+
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            if (rootFrame == null)
+            {
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: 以前中断したアプリケーションから状態を読み込みます
+                }
+
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(340, 400));
+                Window.Current.SizeChanged += (s, ex) =>
+                {
+                    OnWindowSizeChanged(ex.Size);
+                };
+
+                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+
+                OnWindowSizeChanged(new Size(Window.Current.Bounds.Width, Window.Current.Bounds.Height));
+            }
+
+            Window.Current.Activate();
+        }
+
+        private void OnWindowSizeChanged(Size newSize)
+        {
+            var prevState = App.StateManager.CurrentState;
+            bool isChange = StateManager.TryChangeState(newSize.Width);
+            if (isChange)
+            {
+                switch (StateManager.CurrentState)
+                {
+                    case AppState.Mobile:
+                        SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+                        break;
+                    case AppState.Normal:
+                    case AppState.Wide:
+                        SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+                        break;
+                }
+                OnChangeAppState(StateManager.CurrentState, prevState);
+            }
+        }
+
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: アプリケーションの状態を保存してバックグラウンドの動作があれば停止します
+            deferral.Complete();
+        }
+    }
+}
---
+++ BasicTemplate/BasicTemplate/Pages/MainPage.xaml.cs
@@ -0,0 +1,122 @@
+﻿using BasicTemplate.Commons;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
+
+namespace BasicTemplate.Pages
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            //バックボタンをフックする
+            SystemNavigationManager.GetForCurrentView().BackRequested += MainPage_BackRequested;
+
+            //アプリのライフサイクルをフック
+            App.Current.Resuming += Current_Resuming;
+            App.Current.Suspending += Current_Suspending;
+            App.OnChangeAppState += (state, prev) =>
+            {
+                switch (state)
+                {
+                    case AppState.Mobile:
+                        
+                        VisualStateManager.GoToState(this, "MobileState", true);
+                        break;
+                    case AppState.Normal:
+                        
+                        VisualStateManager.GoToState(this, "NormalState", true);
+
+                        break;
+                    case AppState.Wide:
+                        VisualStateManager.GoToState(this, "WideState", true);
+
+                        break;
+                }
+            };
+        }
+
+        //ページが読み込まれた時
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            //HomePageにNavigateする
+            frameContent.Navigate(typeof(HomePage));
+        }
+
+        //検索ボックスで検索しようとしたとき
+        private void suggestBoxSearch_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            var text = args.QueryText;
+        }
+
+        //SplitViewのPaneに作ったナビゲーションボタンをおした時、各Pageに移動
+        private void appButtonSetting_Click(object sender, RoutedEventArgs e)
+        {
+            frameContent.Navigate(typeof(SettingPage));
+            splitView.IsPaneOpen = false;
+        }
+
+        private void appButtonHome_Click(object sender, RoutedEventArgs e)
+        {
+            frameContent.Navigate(typeof(HomePage));
+            splitView.IsPaneOpen = false;
+        }
+
+        private void appButtonFavorite_Click(object sender, RoutedEventArgs e)
+        {
+            frameContent.Navigate(typeof(FavoritePage));
+            splitView.IsPaneOpen = false;
+        }
+
+        //アプリが一時停止しようとしたとき
+        private void Current_Suspending(object sender, Windows.ApplicationModel.SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //ここでアプリのデータや状態を保存するdeferral.Complete()が呼ばれるまではawaitしても待ってくれる
+            //例 ApplicationData.Current.LocalSettings.Values["MyData"] = 1;
+
+            deferral.Complete();
+        }
+
+        //アプリが再開しようとしたとき
+        private void Current_Resuming(object sender, object e)
+        {
+            //ここでアプリのデータや状態を復元する
+            /*例
+            if (ApplicationData.Current.LocalSettings.Values.ContainsKey("MyData"))
+            {
+                var data = ApplicationData.Current.LocalSettings.Values["MyData"];
+            }
+            */
+        }
+
+        //バックボタンが押された時
+        private void MainPage_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            if (frameContent.CanGoBack)
+            {
+                e.Handled = true;
+                frameContent.GoBack();
+            }
+        }
+    }
+}
