be53f636c8e0926e820c226c0df142237ea6db34
garicchi <xgaricchi@gmail.com>
2015-12-08 05:00:28 +0900

    MVVMTemplate追加

---
+++ MVVMTemplate/MVVMTemplate/Pages/HomePage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using MVVMTemplate.Commons;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
+
+namespace MVVMTemplate.Pages
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            this.InitializeComponent();
+        }
+
+        //button1がクリックされたとき
+        private async void button_1_Click(object sender, RoutedEventArgs e)
+        {
+            //このようなコードでテキストボックスの値を取得
+            string text1 = textBox_1.Text;
+
+            //ダイアログで表示
+            var dialog = new MessageDialog(text1);
+            await dialog.ShowAsync();
+        }
+
+        private void button_notify_Click(object sender, RoutedEventArgs e)
+        {
+            NotificationManager.SendBasicToast("テスト通知");
+        }
+    }
+}
---
+++ MVVMTemplate/MVVMTemplate/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMTemplate.ViewModels
+{
+    public class MainViewModel:ViewModelBase
+    {
+        private string _searchQuery;
+
+        public string SearchQuery
+        {
+            get { return _searchQuery; }
+            set { this.Set(ref _searchQuery,value); }
+        }
+
+
+        public MainViewModel()
+        {
+
+        }
+    }
+}
